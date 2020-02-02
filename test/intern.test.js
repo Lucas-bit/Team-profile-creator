f8e5cd0096119c0e704da3ecd85c26d753e1e821
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2020-02-02 13:13:03 -0500

    intern test file passed

--- test/intern.test.js
+++ test/intern.test.js
@@ -1,3 +1,11 @@
-const internRenderer = require("../lib/Intern")
 const Intern = require("../lib/Intern")
 
+describe("Creates intern info", () => {
+    const Joe = new Intern("Joe", 24, "joe@yahoo.com", "OU") 
+
+    test("test intern generator", () => {
+        expect(Joe.getRole()).toEqual
+        ["\"\"Intern\"\""]
+        
+    })
+})
